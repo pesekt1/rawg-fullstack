40ab5c58b8130e391698ce8e4933fb67dd86dca3
Tomas Pesek <pesekt@gmail.com>
2024-09-30 10:52:31 +0200

    first commit

---
+++ rawg-server/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!!");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
