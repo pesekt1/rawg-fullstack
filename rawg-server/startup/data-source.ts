1f6045a7566fc813bb0ed840bed8bdd3936984f3
Tomas Pesek <pesekt@gmail.com>
2024-10-07 12:22:28 +0200

    set up entities path for production

--- rawg-server/startup/data-source.ts
+++ rawg-server/startup/data-source.ts
@@ -3,6 +3,12 @@ import "reflect-metadata";
 
 const connectionString = process.env.MYSQL_URL;
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const entitiesPath = isProduction
+  ? __dirname + "/../../entities/*.js"
+  : __dirname + "/../entities/*.ts";
+
 export const AppDataSource = new DataSource({
   type: "mysql",
   url: connectionString,
