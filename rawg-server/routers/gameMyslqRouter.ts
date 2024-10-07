f13283a5fc6189cfa419fded92abb490c39f13d2
Tomas Pesek <pesekt@gmail.com>
2024-10-07 10:48:55 +0200

    implemented filtering of games by platform, genre, store

--- rawg-server/routers/gameMyslqRouter.ts
+++ rawg-server/routers/gameMyslqRouter.ts
@@ -21,11 +21,48 @@ const gamesRepository = AppDataSource.getRepository(Game);
 
 // Get all games
 gameRouter.get("/", async (req, res) => {
-  const games = await gamesRepository.find({ relations: ["parent_platforms"] });
+  let genreId = req.query.genres ? Number(req.query.genres) : undefined;
+  let storeId = req.query.stores ? Number(req.query.stores) : undefined;
+  let parentPlatformId = req.query.parent_platforms
+    ? Number(req.query.parent_platforms)
+    : undefined;
+
+  const queryBuilder = gamesRepository
+    .createQueryBuilder("game")
+    .leftJoinAndSelect("game.genres", "genres")
+    .leftJoinAndSelect("game.parent_platforms", "parent_platforms")
+    .leftJoinAndSelect("game.stores", "stores");
+
+  if (genreId) {
+    queryBuilder.andWhere(
+      "game.id IN (SELECT games_id FROM games_genres WHERE genres_id = :genreId)",
+      { genreId }
+    );
+  }
+
+  if (storeId) {
+    queryBuilder.andWhere(
+      "game.id IN (SELECT games_id FROM games_stores WHERE stores_id = :storeId)",
+      { storeId }
+    );
+  }
+
+  if (parentPlatformId) {
+    queryBuilder.andWhere(
+      "game.id IN (SELECT games_id FROM games_parent_platforms WHERE parent_platforms_id = :parentPlatformId)",
+      { parentPlatformId }
+    );
+  }
+
+  const games = await queryBuilder.getMany();
+
+  console.log(games);
+
+  // const games = await gamesRepository.find({ relations: ["parent_platforms"] });
 
   const modifinedGames = games.map((game) => ({
     ...game,
-    parent_platforms: game.parent_platforms.map((parent_platform) => ({
+    parent_platforms: game.parent_platforms?.map((parent_platform) => ({
       platform: parent_platform,
     })),
   }));
