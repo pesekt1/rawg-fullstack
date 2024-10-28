c2467c810ecbf0c7314a7ddd510b2b6fa58f0eac
Tomas Pesek <pesekt@gmail.com>
2024-10-28 10:08:21 +0100

    using image placeholder when there is no image

--- rawg-client/src/services/image-url.ts
+++ rawg-client/src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImagePlaceholder from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string | null) => {
-  if (!url) return "";
+  if (!url) return noImagePlaceholder;
   return url.replace("media/games", "media/crop/600/400/games");
 };
 
